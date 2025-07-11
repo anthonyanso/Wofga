ef4018a89cfb1d7ffdda693e102b9f1c9155bb9d
Anthony <anthonyanso2@gmail.com>
2025-07-11 14:30:31 +0100

    feat: configure project for vercel deployment

--- server/index.ts
+++ server/index.ts
@@ -59,8 +59,8 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-app.listen(5000, 'localhost', () => {
-  console.log('Server running');
+  const port = 5001;
+app.listen(port, 'localhost', () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
 })();
